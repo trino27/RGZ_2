93fab1552dd586c07adc38c9b6acdb39b964d833
Artem <artempopovartempopov@gmail.com>
2021-06-13 16:33:35 +0300

    Added main.cpp, main.h, IO.cpp, Convert.cpp.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+﻿#pragma warning(disable : 4996)
+
+#include <iostream>
+#include <Windows.h>
+
+#include "main.h"
+
+using namespace std;
+
+int main() {
+
+	BITMAPFILEHEADER FHeader;
+	BITMAPINFOHEADER IHeader;
+	RGBTRIPLE** Map;
+	
+	FILE *f = fopen("test.bmp", "rb");
+
+	read(Map, FHeader, IHeader, f);
+
+	char** ASCII;
+
+	Convert(Map, ASCII, IHeader.biWidth, IHeader.biHeight);
+
+	Print(ASCII, IHeader.biWidth, IHeader.biHeight);
+
+	return 0;
+}
